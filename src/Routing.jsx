4ecc3096cafbd19ce1355c7083bd4f2fd4674fd6
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-07-09 17:52:53 +0545

    Css Updation

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -22,23 +22,30 @@ function Routing() {
   ];
   return (
     <div className="w-28 h-screen pt-8 pl-0 flex flex-col items-center bg-slate-600">
-    <img className="w-14" src="https://img.hotstar.com/image/upload/v1656431456/web-images/logo-d-plus.svg" alt="" />
-    <button className="flex items-center px-1 py-[1px]  bg-[#ffcc754a] text-[#ffe490] rounded-full mt-5 text-sm justify-center">
-       <span className=""> Subscribe </span> 
-        <MdKeyboardArrowRight className="mt-[2px]" /> 
-    </button>
-      {data.map((elem, index) => {
-        return (
-            <Link className="" to={elem.link} key={index}>
-                <div className="flex items-center gap-2 mt-9 relative cursor-pointer">
-                    <span className="text-xl text-white ">{elem.icon}</span>
-                    <h1 className="text-white absolute top-[-1px] left-8 w-40">
-                        {elem.name}
-                    </h1>
+      <img
+        className="w-14"
+        src="https://img.hotstar.com/image/upload/v1656431456/web-images/logo-d-plus.svg"
+        alt=""
+      />
+      <button className="flex items-center px-1 py-[1px]  bg-[#ffcc754a] text-[#ffe490] rounded-full mt-5 text-sm justify-center">
+        <span className=""> Subscribe </span>
+        <MdKeyboardArrowRight className="mt-[2px]" />
+      </button>
+
+      <div className="relative group cursor-pointer w-full h-[70vh] px-10">  
+        {data.map((elem, index) => {
+            return (
+            <Link className="block" to={elem.link} key={index}>
+                <div className="flex items-center gap-2 mt-9">
+                <span className="text-xl text-white ">{elem.icon}</span>
+                <h1 className="text-white absolute left-[70px] w-40 hidden group-hover:inline-block font-bold text-xl transition transform duration-700">
+                    {elem.name}
+                </h1>
                 </div>
             </Link>
-        );
-      })}
+            );
+        })}
+      </div>
     </div>
   );
 }
