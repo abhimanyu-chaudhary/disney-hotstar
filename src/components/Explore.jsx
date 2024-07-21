b2bc4d909bdf5d8526fa9ada41e0c7cfacfa58ba
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-07-21 21:01:50 +0545

    Update Explore.jsx

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Explore() {
   return (
     <div className='bg-zinc-800 max-w-screen-lg h-full'>
-      Explore
+      <div className='w-screen h-screen bg-indigo-600'>
+        Hello!
+      </div>
     </div>
   )
 }
