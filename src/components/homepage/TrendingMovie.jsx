f6f3e395880ffe787775417c7fa35ead58875f42
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-07-29 21:41:09 +0545

    Trending Movie

--- src/components/homepage/TrendingMovie.jsx
+++ src/components/homepage/TrendingMovie.jsx
@@ -4,6 +4,7 @@ import ApiHandler from './../../api/ApiHandler'
 function TrendingMovie() {
     const url = "https://image.tmdb.org/t/p/original"
     const [movie, setMovie] = useState([]);
+
     useEffect(() => {
         getTrendingMovies();
     })
@@ -11,15 +12,15 @@ function TrendingMovie() {
     const getTrendingMovies = () => {
         ApiHandler.getTrendingMovies.then(res => {
             setMovie(res.data.results);
+            console.log(res.data.results);
         })
     }
 
   return (
     <div className='flex'>
       {movie.map((movie, index) => (
-        <div key={index} className='h-40'>
-            <image src={url + movie.poster_path} alt="movie-poster" />
-        </div>
+        <img className='w-full h-full' src="https://image.tmdb.org/t/p/original/nP6RliHjxsz4irTKsxe8FRhKZYl.jpg" alt="movie-poster" />
+        
       ))}
     </div>
   )
